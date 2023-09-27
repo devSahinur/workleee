961d2e5be832cfdef4cc2f01b325e60cefcd916e
Sahinur Islam <infosahinur@gmail.com>
2023-09-27 18:46:19 +0600

    setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '60%': { transform: 'rotate(10.0deg)' },
+          '100%': { transform: 'rotate(-10deg)' },
+        },
+      },
+      animation: {
+        'waving-astronut': 'wave 7s linear infinite',
       },
     },
   },
-  plugins: [],
+  plugins: [ require('tailwind-scrollbar-hide')],
 }
